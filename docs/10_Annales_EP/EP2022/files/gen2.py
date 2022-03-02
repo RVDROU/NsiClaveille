f6f23956379f577a34ca5b988d06ed545f8833f0
RVDROU <herve.drougard@ac-bordeaux.fr>
2022-03-02 17:37:39 +0100

    maj

--- docs/10_Annales_EP/EP2022/files/gen2.py
+++ docs/10_Annales_EP/EP2022/files/gen2.py
@@ -37,42 +37,43 @@ fichiers_1_2()
 # ### Exercice {0}.1 □
 # !!! example "Exercice {0}.1"
 #     === "Énoncé" 
-#         --8<-- "docs/10_Annales des épreuves pratiques/EP2022/files/{0}_1/enonce.md" 
-# # 
-# #     === "Correction"
-# #         --8<-- "docs/10_Annales des épreuves pratiques/EP2022/files/{0}_1/correction.md" 
+#         --8<-- "docs/T6_6_Epreuve_pratique/files/{0}_1/enonce.md"
+# 
+#     === "Correction"
+#         --8<-- "docs/T6_6_Epreuve_pratique/files/{0}_1/correction.md"
+# 
+#     === "Source Markdown"
+#             --8<-- "docs/T6_6_Epreuve_pratique/files/{0}_1/enonce.md"
 # 
 # 
 # ### Exercice {0}.2 □
 # !!! example "Exercice {0}.2"
 #     === "Énoncé" 
-#         --8<-- "docs/10_Annales des épreuves pratiques/EP2022/files/{0}_2/enonce.md" 
+#         --8<-- "docs/T6_6_Epreuve_pratique/files/{0}_2/enonce.md"
 # 
 #     === "Correction"
-#         --8<-- "docs/10_Annales des épreuves pratiques/EP2022/files/{0}_2/correction.md" 
+#         --8<-- "docs/T6_6_Epreuve_pratique/files/{0}_2/correction.md"
 # 
-# 
+#     === "Sources Markdown"
+#         ```md
+#         --8<-- "docs/T6_6_Epreuve_pratique/files/{0}_2/enonce.md"
 #         ```             
 #         """
-contenu1 = """
-### Exercice {0}.1 □
+
+contenu = """
+### Sujet {0}
 !!! example "Exercice {0}.1"
-    === "Énoncé" 
-"""
+    === "Énoncé du sujet 1" 
+        --8<-- "docs/10_Annales_EP/EP2022/files/{0}_1/enonce.md"
 
-contenu2 = """
-### Exercice {0}.2 □
 !!! example "Exercice {0}.2"
-    === "Énoncé" 
-        
-        """
+    === "Énoncé de l'exercice 2" 
+        --8<-- "docs/10_Annales_EP/EP2022/files/{0}_2/enonce.md"
 
-with open('BNS_2022.md', 'a') as f:
+"""
+with open('EP_NSI_2022.md', 'w') as f:
     for val in lst:
-        file = open ('./'+ val + '_1/' + 'enonce.md','a')
-        f.write(contenu1.format(val))
-        file.write(file.read())
-        file.close()
+        f.write(contenu.format(val))
 
 
 
